e90998cea7a3e4594662fefbd118658bb373cefb
Christian Albrecht <christian.albrecht@mayflower.de>
2021-01-06 14:34:46 +0100

    nightly 86.0a1-20210106093742
    hg: 855ec176a3c2239fe2f166431c96ee7710204b30
    git: ef900cd2258d4c5d968093f612f807d96e6e7c98

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     gecko-dev = {
-      url = github:mozilla/gecko-dev/9570cfcaf724d14b2df91b3f0efdba23ed38a0c5;
+      url = github:mozilla/gecko-dev/ef900cd2258d4c5d968093f612f807d96e6e7c98;
       flake = false;
     };
     nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
@@ -11,7 +11,7 @@
 
   outputs = { self, nixpkgs, gecko-dev }:
     let
-      ffversion = "86.0a1-20210105215658";
+      ffversion = "86.0a1-20210106093742";
 
       system = "x86_64-linux";
 
