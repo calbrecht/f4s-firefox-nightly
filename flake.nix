e69320b59ab4b49c39d82d16650670aec07c2480
Christian Albrecht <christian.albrecht@mayflower.de>
2021-01-14 22:50:52 +0100

    Add nss flake with testsuite and testrunner

--- flake.nix
+++ flake.nix
@@ -6,10 +6,14 @@
       url = github:mozilla/gecko-dev/2a24205479519e70c0574929f45730d285141584;
       flake = false;
     };
+    nss = {
+      url = path:./nss;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
   };
 
-  outputs = { self, nixpkgs, gecko-dev }:
+  outputs = { self, nixpkgs, gecko-dev, nss }:
     let
       ffversion = "86.0a1-20210114083245";
 
@@ -17,56 +21,44 @@
 
       pkgs = nixpkgs.legacyPackages."${system}";
 
-      nss_version_prefix = "#define NSS_VERSION";
-      nss_version = with pkgs.lib; with builtins; toString
-        (match "${nss_version_prefix} .([0-9.]+).*"
-          (toString (filter (hasPrefix nss_version_prefix)
-            (splitString "\n" (readFile (gecko-dev + /security/nss/lib/nss/nss.h))))));
+      overrides = {
+        nss = nss.legacyPackages."${system}".nss;
 
-      nss = pkgs.nss.overrideAttrs (old: {
-        src = gecko-dev + /security;
-        version = nss_version;
-      });
+        rust-cbindgen = pkgs.rust-cbindgen.overrideAttrs (old: rec {
+          name = "rust-cbindgen-${version}";
+          version = "0.16.0";
 
-      overrides = { } //
-        # https://wiki.mozilla.org/NSS:Release_Versions
-        (pkgs.lib.optionalAttrs (pkgs.nss.version != nss_version) { inherit nss; }) //
-        {
-          rust-cbindgen = pkgs.rust-cbindgen.overrideAttrs (old: rec {
-            name = "rust-cbindgen-${version}";
-            version = "0.16.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "eqrion";
+            repo = "cbindgen";
+            rev = "v${version}";
+            sha256 = "sha256-RDqe97smZ4QPFlV4J8eV1ZHOlPKMzUow6/oNuIWgZ90=";
+          };
 
-            src = pkgs.fetchFromGitHub {
-              owner = "eqrion";
-              repo = "cbindgen";
-              rev = "v${version}";
-              sha256 = "sha256-RDqe97smZ4QPFlV4J8eV1ZHOlPKMzUow6/oNuIWgZ90=";
-            };
+          nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.pythonPackages.cython ];
 
-            nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.pythonPackages.cython ];
-
-            cargoDeps = old.cargoDeps.overrideAttrs (pkgs.lib.const {
-              name = "${name}-vendor.tar.gz";
-              inherit src;
-              outputHash = "sha256-MdrXJ/nGxJ1oOHolc599Uee1EWn1iybUCfnc98BfjiE=";
-            });
-
-            checkFlags = [
-              # https://github.com/NixOS/nixpkgs/pull/65303
-              "--skip test_bitfield"
-              "--skip test_expand"
-              "--skip test_expand"
-              "--skip test_expand_default_features"
-              "--skip test_expand_dep"
-              "--skip test_expand_dep_v2"
-              "--skip test_expand_features"
-              "--skip test_expand_no_default_features"
-              "--skip lib_default_uses_debug_build"
-              "--skip lib_explicit_debug_build"
-              "--skip lib_explicit_release_build"
-            ];
+          cargoDeps = old.cargoDeps.overrideAttrs (pkgs.lib.const {
+            name = "${name}-vendor.tar.gz";
+            inherit src;
+            outputHash = "sha256-MdrXJ/nGxJ1oOHolc599Uee1EWn1iybUCfnc98BfjiE=";
           });
-        };
+
+          checkFlags = [
+            # https://github.com/NixOS/nixpkgs/pull/65303
+            "--skip test_bitfield"
+            "--skip test_expand"
+            "--skip test_expand"
+            "--skip test_expand_default_features"
+            "--skip test_expand_dep"
+            "--skip test_expand_dep_v2"
+            "--skip test_expand_features"
+            "--skip test_expand_no_default_features"
+            "--skip lib_default_uses_debug_build"
+            "--skip lib_explicit_debug_build"
+            "--skip lib_explicit_release_build"
+          ];
+        });
+      };
     in
     {
       legacyPackages."${system}" = self.overlay self.legacyPackages."${system}" pkgs;
